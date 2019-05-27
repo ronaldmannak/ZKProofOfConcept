85dba16a8fb870f03eb45fdbf84397a646c8fff8
Ronald Mannak <ronaldmannak@me.com>
2019-05-27 13:31:27 -0700

    Initial Commit

---
+++ ZKProofOfConcept/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ZKProofOfConcept
+//
+//  Created by Ronald "Danger" Mannak on 5/27/19.
+//  Copyright © 2019 A Puzzle A Day. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
