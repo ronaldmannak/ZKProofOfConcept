7644e82ce86c9aa0f4137898bcefe179068ae165
Ronald Mannak <ronaldmannak@me.com>
2019-06-07 00:10:00 -0700

    Expand unit tests

---
+++ ZKProofOfConceptTests/TransactionTests.swift
@@ -0,0 +1,26 @@
+//
+//  TransactionTests.swift
+//  ZKProofOfConceptTests
+//
+//  Created by Ronald "Danger" Mannak on 6/6/19.
+//  Copyright © 2019 A Puzzle A Day. All rights reserved.
+//
+
+import XCTest
+@testable import ZKProofOfConcept
+
+
+class TransactionTests: XCTestCase {
+
+    
+    override func setUp() {
+        
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        
+        super.tearDown()
+    }
+    
+}
